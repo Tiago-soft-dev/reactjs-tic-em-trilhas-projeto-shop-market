b74345d165758d8d78a20efab8638037b08aeb3c
Tiago Soft Dev <tiago.soft.dev@gmail.com>
2024-11-14 21:13:54 -0300

    :lipstick: feat - Estilização componente Card

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,17 +1,23 @@
+import { Product } from "../interfaces/Product"
 import { Button } from "./Button"
 
-export const Card = ()=>{
+export const Card = ({item}: Product)=>{
     return (
-        <div className="bg-white p-4 w-60 rounded-2xl">
-            <div>
-                <img src="http://localhost:5173/public/assets/produtos/tenis.jpg" alt="" />
+        
+        
+        <div className="flex h-96 w-64 flex-col justify-center rounded-2xl bg-white p-4  ">
+            
+            <div className="flex justify-center ">
+                <img src={`http://localhost:5173/public/assets/produtos/${item.image}.jpg`} 
+                     alt={item.name} 
+                     className="h-40 rounded-t-lg object-cover"/>
             </div>
-            <div className="p-4">
+            <div className="p-4 flex flex-col gap-2">
                 <div className="flex justify-center items-center mb-2">
-                    <h3>Nome Produto</h3>
+                    <span className="text-center capitalize font-bold">{item.name}</span>
                 </div>
                 <div className="flex justify-center items-center">
-                    <span>Valor Produto</span>
+                    <span>R$ {item.price}</span>
                 </div>
                 <Button>Adicionar no Carrinho</Button>
                
--- src/app/view/Home.tsx
+++ src/app/view/Home.tsx
@@ -20,11 +20,12 @@ export const Home = ()=>{
 
 
     return(
-        <>
-            {products?.map((item)=>(
-                <Card key={item.id}/>
-            )) }
-            
-        </>
+        <div className="mt-32 h-4/5 w-full flex flex-col items-center justify-center gap-16">
+            <div className="grid h-5/6 w-11/12 grid-cols-4 gap-4 overflow-x-auto">
+                {products?.map((item: ProductProps)=>(
+                    <Card key={item.id} item={item} />
+                )) }
+            </div>
+        </div>
     )
 }
